9fdcf94972eb8c1a9eecd48b16c926fa2c1e1453
ion <ion@hashicorp.com>
2021-11-03 10:47:44 +0100

    fixing README file

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  alias  = "west2"
+}
+
+
+resource "aws_vpc" "vpc-eu-west1" {
+  cidr_block       = "10.202.0.0/16"
+    provider = aws
+  tags = {
+    Name = "ion-web-proj"
+  }
+}
+
+resource "aws_vpc" "vpc-eu-west2" {
+  cidr_block       = "10.202.0.0/16"
+    provider = aws.west2
+
+  tags = {
+    Name = "ion-web-proj"
+  }
+}
